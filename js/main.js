82a0753b2e20f7487903bafca54555b1e047e157
Paul Tang <paulswtang@gmail.com>
2015-02-08 20:54:03 -0500

    Updated landing page to have randomized data transitions.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,108 @@
-var width = 960,
-    height = 500;
+//Streamgraph from FTSE, Hang Seng, Standard and Poors 500
+//borrows heavily from block #4060954
+//(http://bl.ocks.org/mbostock/4060954)
 
-var velocity = [.010, .005],
-    t0 = Date.now();
+/*
+Helpful links:
+  file:///home/ptang11/Documents/Coding/webdesign/personal/paulswtang.github.io/index.html
+  http://bost.ocks.org/mike/path/
+  https://github.com/mbostock/d3/wiki/Tutorials
+  https://github.com/mbostock/d3/wiki/API-Reference
+  http://bost.ocks.org/mike/join/
+To Do:
+  -Make real time data flow from left to right (right to left?)
+  -Tie to financial feeds
+  -Keep data in window bounds
+  -Make responsive: scale to window size appropriately, even on mobile
+*/
 
-var projection = d3.geo.orthographic()
-    .scale(height / 2 - 10);
+//////////////////
+function bumpLayer(n) {
+  // Inspired by Lee Byron's test data generator.
+  function bump(a) {
+    var x = 1 / (.1 + Math.random()),
+        y = 2 * Math.random() - .5,
+        z = 10 / (.1 + Math.random());
+    for (var i = 0; i < n; i++) {
+      var w = (i / n - y) * z;
+      a[i] += x * Math.exp(-w * w);
+    }
+  }
 
+  var a = [], i;
+  for (i = 0; i < n; ++i) a[i] = 0;
+  for (i = 0; i < 5; ++i) bump(a);
+  return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
+}
+//////////////////
+
+var layers = 8, // number of layers
+    samples = 200, // number of samples per layer
+    random = d3.random.normal(0, .2), //N(mu=0,sigma=0.2)-distributed RV
+    //data containers
+    data0 = d3.layout.stack()
+              .offset("wiggle")(
+                d3.range(layers).map(function() { return bumpLayer(samples); }) //range of 8 random bumps
+              ),
+    data1 = d3.layout.stack()
+              .offset("wiggle")(
+                d3.range(layers).map(function() { return bumpLayer(samples); }) //
+              );
+
+//width of stream graphs (n.b. -- need to make responsive!)
+var width = 1350,
+    height = 600;
+
+//x coords are a linear function from number of layers to width
+var x = d3.scale.linear()
+    .domain([0, samples - 1])
+    .range([0, width]);
+
+//y coords are linear scale
+var y = d3.scale.linear()
+    .domain([0, d3.max(
+                  data0.concat(data1),
+                  function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); }
+                )
+    ])
+    .range([height, 0]);
+
+//defines color of stream graphs -- here, from bright grey to dark grey.
+var color = d3.scale.linear()
+    .range(["#eee", "#666"]);
+
+//area generator: see https://github.com/mbostock/d3/wiki/SVG-Shapes#area
+var area = d3.svg.area()
+    .x(function(d) { return x(d.x); })
+    .y0(function(d) { return y(d.y0); })
+    .y1(function(d) { return y(d.y0 + d.y); });
+
+//create an SVG of dimensions <<width x height>>
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var face = svg.selectAll("path")
-    .data(icosahedronFaces)
+//choose path
+svg.selectAll("path")
+    .data(data0)  //get data from data0 stack
   .enter().append("path")
-    .each(function(d) { d.polygon = d3.geom.polygon(d.map(projection)); });
+    .attr("d", area)
+    .style("fill", function() { return color(Math.random()); });
 
-d3.timer(function() {
-  var time = Date.now() - t0;
-  projection.rotate([time * velocity[0], time * velocity[1]]);
 
-  face
-      .each(function(d) { d.forEach(function(p, i) { d.polygon[i] = projection(p); }); })
-      .style("display", function(d) { return d.polygon.area() > 0 ? null : "none"; })
-      .attr("d", function(d) { return "M" + d.polygon.join("L") + "Z"; });
-});
 
-function icosahedronFaces() {
-  var faces = [],
-      y = Math.atan2(1, 2) * 180 / Math.PI;
-  for (var x = 0; x < 360; x += 72) {
-    faces.push(
-      [[x +  0, -90], [x +  0,  -y], [x + 72,  -y]],
-      [[x + 36,   y], [x + 72,  -y], [x +  0,  -y]],
-      [[x + 36,   y], [x +  0,  -y], [x - 36,   y]],
-      [[x + 36,   y], [x - 36,   y], [x - 36,  90]]
-    );
-  }
-  return faces;
+//transition function:
+function transition() {
+  d3.selectAll("path")
+      .data(function() {
+        var d = data1;
+        data1 = data0;
+        return data0 = d3.layout.stack()
+              .offset("wiggle")(d3.range(layers).map(function() { return bumpLayer(samples); }));
+      })
+    .transition()
+      .duration(3500)
+      .attr("d", area);
 }
+//repeat transition every 2.5 seconds
+setInterval(transition,3500);
